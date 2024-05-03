f0e100e95b664bc11fcbdece87c35ec2f4629f50
chunChu <doanthanhchung03nb@gmail.com>
2024-05-03 12:25:29 +0700

    feat: add note card ui

--- Notes-app/frontend/notes-app/src/pages/Home/Home.jsx
+++ Notes-app/frontend/notes-app/src/pages/Home/Home.jsx
@@ -1,10 +1,32 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
+import NoteCard from "../../components/Card/NoteCard";
+import { MdAdd } from "react-icons/md";
 
 const Home = () => {
   return (
     <>
       <Navbar />
+      <div className="container mx-auto">
+        <div className=" grid grid-cols-3 gap-4 mt-8">
+          <NoteCard
+            title="Metting someone on 7th"
+            date="3rd July 2021"
+            content="gặp ai ó vào thứ 7 tuần tới gặp ai ó vào thứ 7 tuần tới gặp ai ó vào thứ 7 tuần tới"
+            tags="#work"
+            isPinned={true}
+            onEdit={() => {}}
+            onDelete={() => {}}
+            onPinNote={() => {}}
+          />
+        </div>
+      </div>
+      <button
+        className="w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10"
+        onClick={() => {}}
+      >
+        <MdAdd className="text-[32px]] text-white" />
+      </button>
     </>
   );
 };

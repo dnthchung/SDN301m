f5e0998f226fd9df12c84b8b4ba5056c5c278467
chunChu <doanthanhchung03nb@gmail.com>
2024-05-05 16:53:17 +0700

    feat: add login api

--- Notes-app/backend/index.js
+++ Notes-app/backend/index.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 
 mongoose.connect(config.connectionString);
 
-//Api step 2
+//Api Create account step 2
 const User = require("./models/user.model");
 
 const express = require("express");
@@ -27,7 +27,7 @@ app.get("/", (req, res) => {
   res.json({ message: "Hello World" });
 });
 
-//Api step 1 : Create account
+//Api Create account step 1 :
 app.post("/create-account", async (req, res) => {
   const { fullName, email, password } = req.body;
 
@@ -72,6 +72,46 @@ app.post("/create-account", async (req, res) => {
     accessToken,
     message: "Account created successfully",
   });
+});
+
+//Api Login step 1 :
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email) {
+    return res.status(400).json({ error: true, message: "Email is required" });
+  }
+
+  if (!password) {
+    return res
+      .status(400)
+      .json({ error: true, message: "Password is required" });
+  }
+
+  // const userInfo = await User.findOne({ email: email, password: password });
+  const userInfo = await User.findOne({ email: email });
+
+  if (!userInfo) {
+    return res.status(400).json({ error: true, message: "User not found!" });
+  }
+
+  if (userInfo.email == email || userInfo.password == password) {
+    const user = { user: userInfo };
+    const accessToken = jwt.sign({ user }, process.env.ACCESS_TOKEN_SECRET, {
+      expiresIn: "36000m",
+    });
+
+    return res.json({
+      error: false,
+      email,
+      accessToken,
+      message: "Login successful",
+    });
+  } else {
+    return res
+      .status(400)
+      .json({ error: true, message: "Invalid email or password" });
+  }
 });
 
 app.listen(8000);

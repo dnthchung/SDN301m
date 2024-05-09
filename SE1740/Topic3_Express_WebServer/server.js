5f3337a76e6946bb76fb0328e33e4468efb3d288
chunChu <doanthanhchung03nb@gmail.com>
2024-05-09 17:28:39 +0700

    add env nodemon

--- SE1740/Topic3_Express_WebServer/server.js
+++ SE1740/Topic3_Express_WebServer/server.js
@@ -1,10 +1,15 @@
 const express = require("express");
 //khởi tạo 1 web server : express
 const app = express();
+require("dotenv").config();
+const PORT = process.env.PORT || 3000;
 
 //định nghĩa 1 route / định tuyến các request từ clien tới đường dẫn gốc của web server
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-export default app;
+//khởi động web server ở cổng 3000
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});

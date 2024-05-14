5fcac5aee69f5a35e118b4c34129b448ba8c286c
chunChu <doanthanhchung03nb@gmail.com>
2024-05-14 14:48:06 +0700

    SDN301m slot 3
    - npm i body-parser (topic 3)
    - npm install http-errors
    - npx express generator

--- SE1740/Topic3_Express_WebServer/server.js
+++ SE1740/Topic3_Express_WebServer/server.js
@@ -1,12 +1,32 @@
 const express = require("express");
+//morgan: log request
+const morgan = require("morgan");
+//import router authen
+const authenRouter = require("./routes/authen.route");
+const createHttpError = require("http-errors");
 //khởi tạo 1 web server : express
 const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 
-//định nghĩa 1 route / định tuyến các request từ clien tới đường dẫn gốc của web server
-app.get("/", (req, res) => {
-  res.send("Hello World");
+//thiết lập các middleware cho web server, "dev" là chỉ áp dụng cho môi trường phát triển, "common" là áp dụng cho môi trường "production
+app.use(morgan("dev"));
+//thiết lập middleware cho web server để có thể đọc dữ liệu từ body của request cuả client
+app.use("/api/auth", authenRouter);
+
+//kiểm soát các lỗi của request xảy ra trên express server
+//truyền vớ vẩn vào thì trả về là lỗi 500 luôn, tránh khi lỗi trả về 1 đống HTML
+app.use(async (req, res, next) => {
+  next(createHttpError.NotFound());
+});
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send({
+    error: {
+      status: err.status || 500,
+      message: err.message,
+    },
+  });
 });
 
 //khởi động web server ở cổng 3000

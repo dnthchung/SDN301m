8df85b74280f18b8e9d1c5fc88ab8376ab2e1ce1
chunChu <doanthanhchung03nb@gmail.com>
2024-05-28 19:50:12 +0700

    feat: add "type" and "nodemon" to run ES6

--- SE1740/Topic6_BTVN_DemoMVC/server.js
+++ SE1740/Topic6_BTVN_DemoMVC/server.js
@@ -2,3 +2,22 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import connectDB from "./database/database";
+
+dotenv.config();
+
+// Tạo đối tượng app để khởi tạo web container
+const app = express();
+app.use(express.json());
+
+// Cấu hình hoạt động routing (định tuyến) các request gửi tới web server
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+//khai báo port cho web
+const port = process.env.PORT || 8080;
+
+app.listen(port, async () => {
+  connectDB();
+  console.log(`Server is running on port ${PORT}`);
+});

897176fbae255b3cf54794aa3aa498aaa5d639e1
dnthchung <doanthanhchung03nb@gmail.com>
2024-07-30 19:36:06 +0700

    Intersection với type và keyword extends trong interface

--- ts/src/App.tsx
+++ ts/src/App.tsx
@@ -1,16 +1,85 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { Count } from "./Count";
 
+//custom type
+/**
+ * Intersection sẽ được dùng khi bạn muốn kết hợp CÁC type khác nhau lại với nhau để tạo ra type mới
+ * - ở type mới như Employee, nó sẽ kế thừa tất cả các thuộc tính của People và thêm các thuộc tính mới
+ */
+type People = {
+  name: string;
+  age: number;
+  location?: string; //- optional
+};
+type Relationship = {
+  sonName: string;
+  daughterName: string;
+};
+
+type Employee = People &
+  Relationship & {
+    jobTitle: string;
+    degree: string;
+  };
+
+interface IPeople {
+  name: string;
+  age: number;
+  location?: string; //- optional
+}
+
+interface IRelationship {
+  sonName: string;
+  daughterName: string;
+}
+
+interface IEmployee extends IPeople, IRelationship {
+  jobTitle: string;
+  degree: string;
+}
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState<number>(0);
+  const [people, setPeople] = useState<People>();
+
+  //type
+  const employee1: Employee = {
+    name: "John Doe",
+    age: 25,
+    location: "Nigeria",
+    jobTitle: "Software Engineer",
+    degree: "BSc",
+    sonName: "John Doe Jr",
+    daughterName: "Jane Doe",
+  };
+
+  //interface
+  const employee2: IEmployee = {
+    name: "John Doe",
+    age: 25,
+    location: "Nigeria",
+    jobTitle: "Software Engineer",
+    degree: "BSc",
+    sonName: "John Doe Jr",
+    daughterName: "Jane Doe",
+  };
 
   function total(num1: number, num2: number): string {
     return String(num1 + num2);
   }
 
+  useEffect(() => {
+    setPeople({
+      name: "John Doe",
+      age: 25,
+      // location: "Nigeria",
+    });
+  }, []);
+
+  console.log(people, employee1, employee2);
   return (
     <>
       <div>

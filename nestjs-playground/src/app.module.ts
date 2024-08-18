5adeaa8657f1fe176a6f3c112bf50dea0b08339c
dnthchung <doanthanhchung03nb@gmail.com>
2024-08-18 16:05:54 +0700

    b15 typeorm - mysql - connect db

--- nestjs-playground/src/app.module.ts
+++ nestjs-playground/src/app.module.ts
@@ -2,13 +2,29 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './modules/products/product.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'nestjs_api1', // tên database
+      entities: [],
+      synchronize: true,
+    }),
     // Add the ProductModule to the imports array
     ProductModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  //add the DataSource to the constructor to use it
+  constructor(private dataSource: DataSource) {
+    // dataSource.connect();
+  }
+}

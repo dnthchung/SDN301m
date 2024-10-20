e8a6bcc075a0c1a1c1635f9f51eacb3e0bcce9d5
dnthchung <doanthanhchung03nb@gmail.com>
2024-10-20 16:23:32 +0700

    Completed Chapter 3

--- plgr-nextjs-base/nextjs-dashboard/app/layout.tsx
+++ plgr-nextjs-base/nextjs-dashboard/app/layout.tsx
@@ -1,9 +1,16 @@
 import "@/app/ui/global.css";
+import { inter } from "@/app/ui/fonts";
+
+/**
+ *
+ * antialiased: This class is used to smooth the font rendering on the page.
+ *
+ */
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }

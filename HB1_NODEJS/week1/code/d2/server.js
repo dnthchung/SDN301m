5a0a7b594b8201d6e93ed72e967abfee3f9395be
hblab-account <chungdthe176077@fpt.edu.vn>
2025-11-12 10:44:46 +0700

    mdf

---
+++ HB1_NODEJS/week1/code/d2/server.js
@@ -0,0 +1,12 @@
+// run: node server.js
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(PORT, () => {
+  console.log(`click to =>  http://localhost:${PORT}`);
+});

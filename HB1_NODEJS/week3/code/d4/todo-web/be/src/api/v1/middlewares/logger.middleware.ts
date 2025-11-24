0d2226cc5513148f41e8256b207c3d2d347ae39f
hblab-account <chungdthe176077@fpt.edu.vn>
2025-11-24 11:39:15 +0700

    mdf

--- HB1_NODEJS/week3/code/d4/todo-web/be/src/api/v1/middlewares/logger.middleware.ts
+++ HB1_NODEJS/week3/code/d4/todo-web/be/src/api/v1/middlewares/logger.middleware.ts
@@ -2,6 +2,6 @@ import { Request, Response, NextFunction } from "express";
 
 export const requestLogger = (req: Request, res: Response, next: NextFunction) => {
   const timestamp = new Date().toISOString();
-  console.log(`ĐÂY LÀ LOG: [${timestamp}] ${req.method} ${req.originalUrl}`);
+  console.log(`LOG: [${timestamp}] ${req.method} ${req.originalUrl}`);
   next();
 };
